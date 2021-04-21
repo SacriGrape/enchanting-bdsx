626348496afa163b06cb4934c0e8abfa9bf4d998
Evan Lee <evan6lee@gmail.com>
2021-04-21 17:31:05 -0500

    fixed formatting

--- enchantItem.ts
+++ enchantItem.ts
@@ -11,7 +11,6 @@ const enchant = hacker.js("?applyEnchant@EnchantUtils@@SA_NAEAVItemStackBase@@W4
 
 export const Enchantment: any = {"protection":0,"fire_protection":1,"feather_falling":2,"blast_protection":3,"projectile_protection":4,"thorns":5,"respiration":6,"depth_strider":7,"aqua_affinity":8,"sharpness":9,"smite":10,"bane_of_arthropods":11,"knockback":12,"fire_aspect":13,"looting":14,"efficiency":15,"silk_touch":16,"unbreaking":17,"fortune":18,"power":19,"punch":20,"flame":21,"infinity":22,"luck_of_the_sea":23,"lure":24,"frost_walker":25,"mending":26,"binding_curse":27,"vanishing_curse":28,"impaling":29,"riptide":30,"loyalty":31,"channeling":32,"multishot":33,"piercing":34,"quick_charge":35,"soul_speed":36}
 
-
 export function enchantInventoryItem(player: ServerPlayer, slot: number, container: ContainerId, enchantment: number, level: number, isUnsafe: boolean) {
     let inventory = player.getInventory();
     let item = inventory.getItem(slot, container);
